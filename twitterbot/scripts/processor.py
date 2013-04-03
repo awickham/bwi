92cf599cffc8dacdca4b220868ea296f6fbab473
Tony <tonyw@cs.utexas.edu>
2013-04-03 01:49:44 -0500

    can now publish to tweet; ensures specified amount of time between tweets

--- twitterbot/scripts/processor.py
+++ twitterbot/scripts/processor.py
@@ -3,18 +3,50 @@ import roslib; roslib.load_manifest('twitterbot')
 import rospy
 from std_msgs.msg import String
 
-import time
-
 tweet_history = []
-time_of_last_tweet = time.time()
+time_of_last_tweet = 0.0
+twitter_task = ""
+DOWN_TIME = 600 # in seconds
+
+
+def pub_tweet():
+    # update global variables
+    global twitter_task
+    # figure out what to tweet
+    tweet = twitter_task # TODO
+    global tweet_history
+    tweet_history.append(tweet)
+    global time_of_last_tweet
+    time_of_last_tweet = rospy.get_time()
+
+    # tweet the tweet
+    pub = rospy.Publisher('tweet', String)
+    rospy.init_node('twitter_processor')
+    rospy.loginfo(tweet)
+    pub.publish(String(tweet))
 
 def callback(data):
-    rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
+    global time_of_last_tweet
+    global twitter_task
+    twitter_task = data.data
+    rospy.loginfo(rospy.get_name() + ": I heard %s" % twitter_task)
+    # determine if we should post another tweet
+    if rospy.get_time() - time_of_last_tweet >= DOWN_TIME:
+	try:
+            pub_tweet()
+        except rospy.ROSInterruptException:
+            pass
 
 def listener():
     rospy.init_node('twitter_processor', anonymous=False)
     rospy.Subscriber("twitter_task", String, callback)
+
+    # initialize global variables
+    global time_of_last_tweet
+    time_of_last_tweet = rospy.get_time()
+
     rospy.spin()
 
 if __name__ == '__main__':
+    # start listening for twitter_task
     listener()
