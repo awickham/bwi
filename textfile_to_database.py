659c87add57f00a6e6fe175e298941b90c5500e8
tony-w <tonyw@cs.utexas.edu>
2013-04-08 23:59:50 -0500

    now parses adverbs and hashtags

--- textfile_to_database.py
+++ textfile_to_database.py
@@ -37,6 +37,59 @@ def get_adjectives(f):
         line = f.readline().rstrip('\n')
     return adjectives
 
+def get_adverbs(f):
+    '''Maps emotions to adjectives'''
+    adverbs = {}
+    current_emotion = ""
+    line = f.readline().rstrip('\n')
+    while line != "END ADVERBS":
+        #ignore blank lines
+        if line == "":
+            line = f.readline().rstrip('\n')
+            continue
+        if line[:10] == "Emotion = ":
+            current_emotion = line[10:]
+            #create dictionary entry for this emotion
+            adverbs[current_emotion] = []
+        else:
+            #add this line to the list of definitions for the emotion
+            adverbs[current_emotion].append(line)
+        line = f.readline().rstrip('\n')
+    return adverbs
+
+def get_hashtag_generic(f):
+    '''Lists generic hashtags'''
+    hashtag_generic = []
+    line = f.readline().rstrip('\n')
+    while line != "END HASHTAG GENERIC":
+        #ignore blank lines
+        if line == "":
+            line = f.readline().rstrip('\n')
+            continue
+        hashtag_generic.append(line)
+        line = f.readline().rstrip('\n')
+    return hashtag_generic
+
+def get_hashtag_emotion(f):
+    '''Maps emotions to hashtags'''
+    hashtag_emotion = {}
+    current_emotion = ""
+    line = f.readline().rstrip('\n')
+    while line != "END HASHTAG EMOTION":
+        #ignore blank lines
+        if line == "":
+            line = f.readline().rstrip('\n')
+            continue
+        if line[:10] == "Emotion = ":
+            current_emotion = line[10:]
+            #create dictionary entry for this emotion
+            hashtag_emotion[current_emotion] = []
+        else:
+            #add this line to the list of definitions for the emotion
+            hashtag_emotion[current_emotion].append(line)
+        line = f.readline().rstrip('\n')
+    return hashtag_emotion
+
 def get_tweet_dictionary(f):
     '''Maps keys to tweets'''
     tweet_dictionary = {}
@@ -79,6 +132,15 @@ while line != "END SCRIPT":
     elif line == "ADJECTIVES":
         adjectives = get_adjectives(f)
         print(adjectives)
+    elif line == "ADVERBS":
+        adverbs = get_adverbs(f)
+        print(adverbs)
+    elif line == "HASHTAG GENERIC":
+        hashtag_generic = get_hashtag_generic(f)
+        print(hashtag_generic)
+    elif line == "HASHTAG EMOTION":
+        hashtag_emotion = get_hashtag_emotion(f)
+        print(hashtag_emotion)
     elif line == "TWEET DICTIONARY":
         tweet_dictionary = get_tweet_dictionary(f)
         print(tweet_dictionary)
