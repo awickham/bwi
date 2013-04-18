7b33b1cc0e19275b9fbedda481ebf98c14e78def
Ryan Zabcik <ryanzabcik@gmail.com>
2013-04-17 22:36:41 -0500

    finished taskmon

--- twitterbot/scripts/taskmon.py
+++ twitterbot/scripts/taskmon.py
@@ -1,36 +1,43 @@
-import roslib; roslib.load_manifest('twitterbot')
+import roslib; roslib.load_manifest("twitterbot")
 import rospy
 from std_msgs.msg import String
 import twitter
 import rosnode
 import rosgraph
-
+import time
 from sets import Set
-Set runningNodes = Set([])
+runningNodes = set()
 
 def talker():
-    pub = rospy.Publisher('taskmon_talker', String)
-    rospy.init_node('taskmon')
+    pub = rospy.Publisher("twitter_task", String)
+    rospy.init_node("taskmon")
     while not rospy.is_shutdown():
-        newRunningNodes = Set(rosnode.get_node_names())
-        for newnode in newRunningNodes:
-            if newnode not in runningNodes:
-                pub.publish
-
+        updateState()
+        time.sleep(10)
 
 def updateState():
-    """Taken from rosnode source to be a little less general and more efficient"""
-    master = rosgraph.master('/rosnode')
+    # Taken from rosnode source to be a little less general and more efficient
+    master = rosgraph.master("/rosnode")
     try:
         state = master.getSystemState()
     except socket.error:
         raise ROSNodeIOException("Unable to communicate with master!")
-    nodes = Set([])
+    nodes = set()
     for s in state: # one of publishers, subscribers, or services
         for t, l in s: # topics and participants of topics
+            for node in l:
+                if not node in runningNodes:
+                    pub.publish("new node %s" % node)
+                    if node == "segbot_gazebo":
+                        pub.publish("turned_on")
+                    runningNodes.append
+    for node in runningNodes:
+        if not node in nodes:
+            pub.publish("old node %s vanished" % node)
+            if node == "segbot_gazebo":
+                pub.publish("turned_off")
+            runningNodes.remove(node)
 
-            
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     talker()
