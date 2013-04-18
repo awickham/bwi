e62de35bc4fd141368d9f2541199b250e0064bb2
Ryan Zabcik <ryanzabcik@gmail.com>
2013-04-18 00:49:28 -0500

    prints nodes, still some bugs to work out

--- twitterbot/scripts/taskmon.py
+++ twitterbot/scripts/taskmon.py
@@ -1,36 +1,51 @@
-import roslib; roslib.load_manifest('twitterbot')
+#!/usr/bin/env python
+import roslib; roslib.load_manifest("twitterbot")
 import rospy
 from std_msgs.msg import String
 import twitter
 import rosnode
 import rosgraph
-
+import rosgraph.masterapi
+import time
 from sets import Set
-Set runningNodes = Set([])
+runningNodes = set()
+pub = rospy.Publisher("twitter_task", String)
 
 def talker():
-    pub = rospy.Publisher('taskmon_talker', String)
-    rospy.init_node('taskmon')
+    global pub
+    rospy.init_node("taskmon")
     while not rospy.is_shutdown():
-        newRunningNodes = Set(rosnode.get_node_names())
-        for newnode in newRunningNodes:
-            if newnode not in runningNodes:
-                pub.publish
-
+        updateState()
+        time.sleep(10)
 
 def updateState():
-    """Taken from rosnode source to be a little less general and more efficient"""
-    master = rosgraph.master('/rosnode')
+    # Taken from rosnode source to be a little less general and more efficient
+    global pub
+    global runningNodes
+    master = rosgraph.masterapi.Master('rosgraph', master_uri="http://128.83.122.157:11311/")
     try:
         state = master.getSystemState()
     except socket.error:
         raise ROSNodeIOException("Unable to communicate with master!")
-    nodes = Set([])
+    nodes = set()
     for s in state: # one of publishers, subscribers, or services
         for t, l in s: # topics and participants of topics
+            for node in l:
+                if not node in runningNodes:
+                    pub.publish("new node %s appeared" % node)
+                    if node == "segbot_gazebo":
+                        pub.publish("turned_on")
+                    runningNodes.add(node)
+    nodesToRemove = []
+    for node in runningNodes:
+        if not node in nodes:
+            pub.publish("old node %s vanished" % node)
+            if node == "segbot_gazebo":
+                pub.publish("turned_off")
+            nodesToRemove.append(node)
+    for node in nodesToRemove:
+        runningNodes.remove(node)
 
-            
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     talker()
